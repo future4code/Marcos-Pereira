30476976088841c807e396e1f889c2f41e407891
MarcosMunarim <marcosvmunarim@gmail.com>
2021-10-08 22:48:52 -0300

    Exercício 15 feito

--- quinzena3/revisao-js/exercicios.js
+++ quinzena3/revisao-js/exercicios.js
@@ -142,7 +142,17 @@ function segundoMaiorEMenor(array) {
 
 // EXERCÍCIO 11
 function ordenaArray(array) {
-  
+  const len = array.length
+  for (let i = 0;i < len;i++) {
+    for (let j = 0;j < len;j++) {
+      if (array[j] > array[j + 1]) {
+        let tmp = array[j]
+        array[j] = array[j + 1]
+        array[j + 1] = tmp
+      }
+    }
+  }
+  return array
 }
 
 // EXERCÍCIO 12
