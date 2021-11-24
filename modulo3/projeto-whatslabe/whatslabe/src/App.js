af125e22a5744a3185c4873bece31ac2fd77bc6a
MarcosMunarim <marcosvmunarim@gmail.com>
2021-11-23 22:13:08 -0300

    Exercício rend-condicional-form

---
+++ modulo3/projeto-whatslabe/whatslabe/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ContainerInputs from "./components/ContainerInputs";
+
+
+class App extends React.Component() {
+
+  render (){
+
+    return (
+      <div>
+        <ContainerInputs />
+      </div>
+    );
+  }
+  
+}
+
+export default App;
