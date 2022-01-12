758fd0ef27f1b07357e7947095ddae2b4d8341f3
MarcosMunarim <marcosvmunarim@gmail.com>
2022-01-11 22:36:50 -0300

    Astromatch

---
+++ modulo4/projeto-astromatch/astromatch/src/components/Match.js
@@ -0,0 +1,25 @@
+import React,{useEffect , useState}from "react"; 
+import axios from "axios"
+import styled from "styled-components"
+import Cabeca from "./Cabeca"
+
+const Box = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: space-between;
+width: 420px;
+height: 80vh;
+border: 1px solid black; 
+border-radius: 5px;
+`
+
+function Match() {
+  return (
+    <Box>
+    <Cabeca/>
+    </Box>
+  );
+}
+
+export default Match;
