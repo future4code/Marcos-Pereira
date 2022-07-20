2045fa1b788f988f3365c47ecf2ed9fdbcb7f5f5
MarcosMunarim <marcosvmunarim@gmail.com>
2022-07-19 21:26:10 -0300

    Exercícios estrutura de dados

---
+++ modulo8/Estrutura-de-dados/src/LinkedList/LinkedListNode.ts
@@ -0,0 +1,11 @@
+class LinkedListNode {
+  constructor(public value: any, public next: LinkedListNode | null = null) {}
+  getData() {
+    return this.value;
+  }
+  getNext() {
+    return this.next;
+  }
+}
+
+export { LinkedListNode };
