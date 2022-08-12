e5f0f1d87cd622d56ccee38ba016bc23b2388b26
MarcosMunarim <marcosvmunarim@gmail.com>
2022-08-11 21:12:19 -0300

    Case Amaro

---
+++ cases/case3/Amaro/src/index.ts
@@ -0,0 +1,16 @@
+import { AddressInfo } from "net";
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+const server = app.listen(process.env.PORT || 3000, () => {
+  if (server) {
+    const address = server.address() as AddressInfo;
+    console.log(`Servidor rodando em http://localhost:${address.port}`);
+  } else {
+    console.error(`Falha ao rodar o servidor.`);
+  }
+});
